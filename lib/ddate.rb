e292d5f4745589138a6f07625216cbd5d2df7556
Nick Stielau <nick.stielau@gmail.com>
2009-08-13 12:39:09 -0700

    Removing non-pages stuff.

--- lib/ddate.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'ddate/discordian_date'
-
-module Ddate
-  VERSION = '0.0.1'
-end
